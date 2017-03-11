28d4690d93da4e80ef3169a59bf58426410e65e1
David Miller <davidfmiller@me.com>
2017-03-10 20:47:06 -0700

    fix for node check

--- fullscreen.js
+++ fullscreen.js
@@ -90,7 +90,7 @@
 
     this.node = typeof node == 'string' ? document.querySelector(node) : node;
 
-    if (! (node instanceof HTMLElement)) {
+    if (! (this.node instanceof HTMLElement)) {
       throw Error('Invalid FullScreen node <' + node + '>');
     }
 
