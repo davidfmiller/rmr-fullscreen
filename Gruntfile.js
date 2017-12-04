f748a234d455fb03ab69560db7c25c9608dc78d9
David Miller <davidfmiller@me.com>
2017-12-03 20:40:21 -0800

    eslint gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,9 @@
-/*jshint esnext:true */
-/* globals module */
- 
+
 module.exports = function(grunt) {
- 
   'use strict';
- 
+
   grunt.initConfig({
- 
+
     pkg: grunt.file.readJSON('package.json'),
 /*
     uglify: {
@@ -15,41 +12,40 @@ module.exports = function(grunt) {
           mangle: true
         },
         files: {
-          'site/assets/js/treeera.js' : ['src/scripts/backdrop.js', 'src/scripts/popover.js', 'src/scripts/treeera.js']
+          'site/assets/js/treeera.js': ['src/scripts/backdrop.js', 'src/scripts/popover.js', 'src/scripts/treeera.js']
         }
       }
     },
 */
-    compass : {
-      dist : {
-        options : {
-          sassDir : 'src',
-          cssDir : 'docs/build',
-          environment : 'production',
-          outputStyle : 'compressed'
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'src',
+          cssDir: 'docs/build',
+          environment: 'production',
+          outputStyle: 'compressed'
         }
       }
     },
- 
-    watch : {
-      css : {
-        files : ['src/*.scss'],
-        tasks : ['compass']
+
+    watch: {
+      css: {
+        files: ['src/*.scss'],
+        tasks: ['compass']
       }
-      /*,
-      js : {
-        files : ['src/scripts/*.js'],
-        tasks : ['uglify']
+      /* ,
+      js: {
+        files: ['src/scripts/*.js'],
+        tasks: ['uglify']
       }
       */
     }
   });
- 
+
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
- 
- 
+
   grunt.registerTask('default', ['compass', 'uglify']);
 };
