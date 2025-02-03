eb9193c8451b95e46e4f8b872189763088180e7f
David Miller <davidfmiller@me.com>
2025-02-02 22:14:33 -0700

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ const config = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['es2015', { modules: false }]
+              ['es2015' ]
             ]
           }
         }]
